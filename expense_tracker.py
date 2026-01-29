1590de0d0543d07d6c99c53482a88d9271a6fa00
Willis Otieno <otienowilo@gmail.com>
2026-01-29 06:26:07 +0300

    day 4 added budget alerts with if/else

--- expense_tracker.py
+++ expense_tracker.py
@@ -21,3 +21,23 @@ print(f'item:{expense_name}')  #float insertd variable into text
 print(f'amount: ksh{expense_amount}')
 print(f'total expenses so far:ksh {sum(expenses_list)}')
 print('thank you')
+
+#Addition to expense tracker
+budget=1000
+
+#BOLEAN AND COMPARISON {free code camp boleans and conditionals}
+is_over_budget = expense_amount >budget  # this evaluates to True of False
+print(f'is over budget ?{is_over_budget}')
+
+#IF/ELSE 
+if expense_amount >budget:
+    print('OVER BUDGET! considfe reducing this expense')
+elif expense_amount== budget:
+    print("Exactly at budget limit")
+else:
+    #logical operators
+
+     if expense_amount > 0 and expense_amount < budget*0.5:
+        print("Good! this uses less than 50% of your budget")
+     else:
+        print("Within budget")
